1d8a8df7701c7db9f6b3337ad940d265e9c78b02
Arman <lordarman2006@gmail.com>
2026-04-06 23:12:07 +0300

    2301
    Александру Вячеславовичу

---
+++ 2301/src/Task1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Task1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double a = sc.nextDouble();
+
+        double S = a * a;
+
+        System.out.println("S = " + S);
+    }
+}
