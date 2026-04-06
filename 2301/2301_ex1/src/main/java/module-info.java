a82351168248f828429bee1792d9994860fd25d5
Arman <lordarman2006@gmail.com>
2026-04-06 23:09:00 +0300

    Delete 2301 directory

--- 2301/2301_ex1/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example._2301_ex1_ {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example._2301_ex1_ to javafx.fxml;
-    exports com.example._2301_ex1_;
-}
--- 2301/2301_ex4/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example._2301_ex4_ {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example._2301_ex4_ to javafx.fxml;
-    exports com.example._2301_ex4_;
-}
